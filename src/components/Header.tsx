b3c9004e84f958ddd9910b95b1ac671f56c253e8
NishatFariza <nishatfarzia78@gmail.com>
2022-11-05 06:47:57 +0600

    wip:header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center bg-yellow-700 h-[40px] px-8'>
           <div>
-              <h3>Jk</h3>
+              <h3 className='text-2xl text-white font-semibold'>Jk</h3>
           </div>
           <div>
               <Link href="">
