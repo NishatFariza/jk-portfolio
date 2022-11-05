068eea7fd80794b5f079720919aac7d20623ec76
NishatFariza <nishatfarzia78@gmail.com>
2022-11-05 07:33:49 +0600

    add:hero component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,29 +3,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-      <div className='flex justify-between items-center bg-yellow-700 h-[40px] px-8'>
-          <div>
-              <h3 className='text-2xl text-white font-semibold'>Jk</h3>
-          </div>
-          <div>
-              <Link href="">
-                About
-              </Link>
-              <Link href="">
-                Skill
-              </Link>
-              <Link href="">
-                Portfolio
-              </Link>
-              <Link href="">
-                Contact
-              </Link>
-              <Link href="">
-                Blog
-              </Link>
-          </div>
-     </div>
-  )
+    <div className="flex justify-between items-center bg-[#111A28] h-[40px] px-8">
+      <div>
+        <h3 className="text-2xl text-white font-semibold">Jk</h3>
+      </div>
+      <div className='text-white flex gap-4'>
+        <Link href="">About</Link>
+        <Link href="">Skill</Link>
+        <Link href="">Portfolio</Link>
+        <Link href="">Contact</Link>
+        <Link href="">Blog</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Hero />
     </div>
   )
 }
