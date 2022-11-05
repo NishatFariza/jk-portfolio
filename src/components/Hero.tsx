1a9aa6d8e5e4f99050f7f00a21f3a6ae7fbbc958
NishatFariza <nishatfarzia78@gmail.com>
2022-11-06 04:35:50 +0600

    add:type animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 const Hero = () => {
-  return (
-      <div>
-          
-    </div>
-  )
-}
+  const typedRef = useRef(null);
 
-export default Hero
+  useEffect(() => {
+    const typed = new Typed(typedRef.current!, {
+      strings: ["Hello, I am Rayhan", "I love to watch tutorial!!"],
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      smartBackspace: true,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  });
+
+  return (
+    <div className="text-3xl font-semibold">
+      <span ref={typedRef}></span>
+    </div>
+  );
+};
+
+export default Hero;
