354cf269198aaa63003fb80693c871f24a5faafc
NishatFariza <nishatfarzia78@gmail.com>
2022-11-05 12:55:21 +0600

    add:about component

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>This is JK About</div>
+  )
+}
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <About />
     </div>
   )
 }
