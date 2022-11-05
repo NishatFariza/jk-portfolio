73777d41f99a11af4f808c1c2e36a9e8b78ac278
NishatFariza <nishatfarzia78@gmail.com>
2022-11-05 13:10:30 +0600

    add:footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Skill from '../components/Skill'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
 import Blog from '../components/Blog'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Portfolio />
       <Contact />
       <Blog />
+      <Footer />
     </div>
   )
 }
