47e3fe5d9d4061d0985dcb540c7b198912d8d013
NishatFariza <nishatfarzia78@gmail.com>
2022-11-07 09:05:45 +0600

    wip:blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Skill from '../components/Skill'
-import Portfolio from '../components/Portfolio'
-import Contact from '../components/Contact'
-import Blog from '../components/Blog'
-import Footer from '../components/Footer'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skill from "../components/Skill";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+import Blog from "./Blog";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className="bg-[#111A28] text-white">
-      <div className='px-28'>
+      <div className="px-28">
         <Header />
         <Hero />
         <About />
         <Skill />
         <Portfolio />
         <Contact />
-        
       </div>
-        <Footer />
+      <Footer />
     </div>
   );
 }
