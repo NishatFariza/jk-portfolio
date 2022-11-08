e2f1a0988f4505eb2718213bb918b8298a7a5927
NishatFariza <nishatfarzia78@gmail.com>
2022-11-08 16:16:33 +0600

    wip:my hobby component

---
+++ src/components/MyHobby.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyHobby = () => {
+  return (
+    <div>MyHobby</div>
+  )
+}
+
+export default MyHobby
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Portfolio from "../components/Portfolio";
 import Contact from "../components/Contact";
 import Blog from "./Blog";
 import Footer from "../components/Footer";
+import MyHobby from "../components/MyHobby";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Hero />
         <About />
         <Skill />
+        <MyHobby />
         <Portfolio />
         <Contact />
       </div>
