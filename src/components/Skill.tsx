4f6f8706607dbaf8e5d0fa706bb80f73c992663b
NishatFariza <nishatfarzia78@gmail.com>
2022-11-09 16:53:02 +0600

    wip:skill tabs

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,14 +1,104 @@
-import React from 'react'
+import React from "react";
+import SkillPanel from "./SkillPanel";
 
 const Skill = () => {
   return (
-    <div>
-      <div>
-        <h4>Web</h4>
-        <span>90%</span>
+    <div className="py-20">
+      <SkillPanel />
+      <div className="grid grid-cols-3 gap-6 mt-4">
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
+        <div className="bg-[#1D293A] rounded-md">
+          <div className="flex items-center justify-between px-4 py-4">
+            <h4>Web</h4>
+            <span>90%</span>
+          </div>
+          <div
+            style={{ width: "90%" }}
+            className="h-2 bg-green-600 rounded-md"
+          ></div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skill
+export default Skill;
--- src/components/SkillPanel.tsx
+++ src/components/SkillPanel.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react'
 
-const HobbyPanel = () => {
+const SkillPanel = () => {
   return (
-    <div className="mb-4">
+    <div className="mt-12">
       <div className="flex items-center mt-20 gap-1">
         <p className="text-[#DCDDDF] text-xl font-semibold bg-[#1D293A] py-1 px-2 rounded-md uppercase">
-          LOVETODO
+          MySkill
         </p>
         <div className="w-[60px] h-1 bg-[#DCDDDF] rounded"></div>
       </div>
     </div>
   );
-};
+}
 
-export default HobbyPanel;
+export default SkillPanel
