00b78a315a989bd655d3dc14b2b25a663bd21ff3
NishatFariza <nishatfarzia78@gmail.com>
2022-11-10 13:19:56 +0600

    refector:about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div id="about" className="w-8/12 mx-auto ">
       <div className=" text-center ">
         <AboutPanel />
-        <p className="leading-loose">
+        <p className="leading-loose text-justify">
           I am a second-year computer science student, aspiring Software Engineer, and Data Scientist with one year of experience performing Python, SQL, and R, in the software engineering industry at the University of North Carolina at Chapel Hill as an Undergraduate Research Assistant. I primarily helped with cleansing and sorting through datasets. My ambition is to one day help companies become more efficient in providing their products to consumers through data and
           developed code. My projects are built from the ground up, from planning and designing to solving real-life problems with code.
         </p>
