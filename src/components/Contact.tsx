f2b686a9291a50faaccd7915f055f91081b471bd
NishatFariza <nishatfarzia78@gmail.com>
2022-11-10 13:27:01 +0600

    refector:contact style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex h-96 justify-between items-center">
-      <div className="w-6/12">
-        <p className="mb-5 leading-loose">
+    <div className="flex py-28 justify-between items-center gap-10">
+      <div className="w-7/12">
+        <p className="mb-8 leading-loose">
           I am thrilled about opportunities and challenges that allow me to utilize my skills so I can help turn data and numbers into easily quantifiable metrics. I did love to be able to use my insights to help drive growth within a team and a company, if this sounds like you please feel free to contact me!
         </p>
         <button className="border-none bg-green-600 py-3 px-5 rounded-md text-white text-xl">
           Contact Me
         </button>
       </div>
-      <div className="w-5/12">
+      <div className="w-4/12">
         <div className="mb-3">
           <h5 className="text-2xl text-stone-200 font-semibold ">Email</h5>
           <p className="text-slate-400">
