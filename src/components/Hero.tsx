42d261626ed72434ec2d39ca52d2559c89401426
NishatFariza <nishatfarzia78@gmail.com>
2022-11-10 12:42:34 +0600

    refector:hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
   useEffect(() => {
     const typed = new Typed(typedRef.current!, {
-      strings: ["JJ Krasnick", "I use data to find solutions"],
+      strings: ["JJ Krasnick", "I use data to find solutions!!"],
       startDelay: 300,
       typeSpeed: 100,
       backSpeed: 100,
