f3763cd02765e1ac9914b46732fe8d4d498b40d0
NishatFariza <nishatfarzia78@gmail.com>
2022-11-12 02:40:54 +0600

    wip:portfolio

---
+++ src/components/PortfolioPanel.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PortfolioPanel = () => {
+  return (
+    <div className="mt-12 mb-7">
+      <div className="flex items-center mt-20 gap-1">
+        <p className="text-[#DCDDDF] text-xl font-semibold bg-[#1D293A] py-1 px-2 rounded-md uppercase">
+          Portfolio
+        </p>
+        <div className="w-[60px] h-1 bg-[#DCDDDF] rounded"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioPanel
