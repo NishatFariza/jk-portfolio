d50676631986f0ba8195fc08061a25e8eef0d499
NishatFariza <nishatfarzia78@gmail.com>
2022-11-19 14:39:44 +0600

    refector:nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core'
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>JJ Portfolio</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <MantineProvider
-        withGlobalStyles  
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  );
-    
+  return <Component {...pageProps} />
 }
